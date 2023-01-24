25d6c26e68a6b0510f8626e9c2aaa05442eab5c3
Atharva123987 <atharva.amberkar@gmail.com>
2023-01-24 12:42:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var player =0;
+var move = 0;
+
+document.querySelectorAll(".cells").addEventListener("click", playMove(move));
+document.querySelectorAll(".cells").addEventListener("click", playMove(move));
+
+function playMove(move){
+    for(var i =1;i<=9;i++){
+        console.log(document.querySelectorAll(".${i}"));
+    }
+    
+}

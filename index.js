0ba1b1d7c633135e6d9f0293f0b3979ca12aaaea
Atharva123987 <atharva.amberkar@gmail.com>
2023-01-28 13:05:52 +0530

    Added timer and github link

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var player = 0;
 var filled =["","","","","","","","","",""]
 var turns=0;
 var stat=0;
+var count=14;
+
+var time = document.getElementById("timer");
 
         function move(c){
             
@@ -10,27 +13,35 @@ var stat=0;
                 image[0].style.display="block";
                 player = 1;
                 filled[c[2]] = 'x';
-                turns++;
+                turns++;count = 15;
+               
             }
             else if(player == 1 && filled[c[2]] == "" && stat==0){
                 var image =document.querySelectorAll(c + " .o");
                 image[0].style.display="block";
                 player = 0;
                 filled[c[2]] = 'o';
-                turns++;
+                turns++;count = 15;
+                
             }
             else{console.log("Already filled!");}
-            var check=0;
+            
             if((filled[1] == 'x' && filled[2] == 'x' && filled[3] == 'x') ||(filled[1] == 'o' && filled[2] == 'o' && filled[3] == 'o') ||(filled[4] == 'x' && filled[5] == 'x' && filled[6] == 'x') ||(filled[4] == 'o' && filled[5] == 'o' && filled[6] == 'o')||(filled[7] == 'x' && filled[8] == 'x' && filled[9] == 'x') ||(filled[7] == 'o' && filled[8] == 'o' && filled[9] == 'o')|| (filled[1] == 'x' && filled[5] == 'x' && filled[9] == 'x') ||(filled[1] == 'o' && filled[5] == 'o' && filled[9] == 'o') || (filled[3] == 'x' && filled[5] == 'x' && filled[7] == 'x') ||(filled[3] == 'o' && filled[5] == 'o' && filled[7] == 'o') || (filled[1] == 'x' && filled[4] == 'x' && filled[7] == 'x') ||(filled[1] == 'o' && filled[4] == 'o' && filled[7] == 'o')||(filled[2] == 'x' && filled[5] == 'x' && filled[8] == 'x') ||(filled[2] == 'o' && filled[5] == 'o' && filled[8] == 'o')|| (filled[3] == 'x' && filled[6] == 'x' && filled[9] == 'x') ||(filled[3] == 'o' && filled[6] == 'o' && filled[9] == 'o')) {
                 var gg =document.querySelectorAll(".gameover");
                 gg[0].style.display="inline";
                 stat=1;
+                time.style.display="none";
             }
             if(turns==9){
                 var draw = document.querySelectorAll(".draw");
                 draw[0].style.display="inline";
                 stat=1;
+                time.style.display="none";
             }
+            if(turns == 1){
+                timer();
+            }
+            
             
             
         }
@@ -60,15 +71,33 @@ function reset(){
         location.reload()
     
 }
-// var count=15;
-// function timer(){
-//     setInterval(() =>{
-//         if(count<0) return;
-//             console.log(count);
-//             count--;
 
-//     }
-//         ,1000
-//     )
-// }
-// timer();
+function timer(){
+   
+    count = 14;
+    setInterval(() =>{
+        // if(stat==1){
+            
+        //     time.style.display="none";
+        //     return;
+        // }
+        if(count<1 && !stat){
+            time.style.display="none";
+            var gg =document.querySelectorAll(".gameover");
+                gg[0].style.display="inline";
+            return;
+        }
+        else if(turns==9){
+            time.style.display="none";
+            return;
+        }
+        
+            time.innerHTML = "Time: "+count;
+            console.log(count);
+            count--;
+            
+    }   
+        ,1000
+    )
+}
+
